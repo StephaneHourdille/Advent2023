f4d237332a36c6dcf7a630cc92700973fa16b5cc
stephane hourdille <stephane.hourdille@gmail.com>
2024-05-05 10:09:34 +0200

    start day01

---
+++ Day01/Day01.cs
@@ -0,0 +1,15 @@
+using System.Text.RegularExpressions;
+
+class day01{
+
+
+
+public static void Part01 (){
+     var input = File.ReadAllLines(@".\input.txt");
+     
+}
+
+public static void Part02 (){
+
+}
+}
