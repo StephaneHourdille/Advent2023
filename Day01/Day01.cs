dbc935cc3576e5392bf590ae273c06a58e91ec15
stephane hourdille <stephane.hourdille@gmail.com>
2024-05-05 12:22:56 +0200

    part 1

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -1,15 +1,20 @@
 using System.Text.RegularExpressions;
 
-class day01{
+public class Day01{
+    public Day01()
+    {
+    }
 
+    public void Part01 (){
+     var result = 0;
+     var input = File.ReadAllLines(@"./Day01/input.txt");
+     foreach(var line in input){
+       result+= int.Parse($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");
+     }
+     Console.WriteLine(result);
+    }
 
+    public void Part02 (){
 
-public static void Part01 (){
-     var input = File.ReadAllLines(@".\input.txt");
-     
-}
-
-public static void Part02 (){
-
-}
+    }
 }
